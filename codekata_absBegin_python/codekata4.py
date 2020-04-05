83e7270234bcec9605caea999c91ab1aa69b84ce
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2020-04-06 01:48:59 +0530

    Create codekata4.py

---
+++ codekata_absBegin_python/codekata4.py
@@ -0,0 +1,18 @@
+Using the method of looping, write a program to print the table of 9 till N in the format as follows:
+(N is input by the user)
+
+9 18 27...
+
+Print NULL if 0 is input
+
+
+
+a = int(input())
+
+for i in range(1,a+1):
+    if i == a:
+        print(9*i)
+    else:
+        print(9*i,end=" ")
+    
+        
