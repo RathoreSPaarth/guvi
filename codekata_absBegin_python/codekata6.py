e686495b413078f15567763ecf88fda68dd1ce2b
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2020-04-06 02:07:06 +0530

    Create codekata6.py

---
+++ codekata_absBegin_python/codekata6.py
@@ -0,0 +1,12 @@
+You are provided with a number "N", Find the Nth term of the series: 1, 4, 9, 25, 36, 49, 64, 81, .......
+
+(Print "Error" if N = negative value and 0 if N = 0).
+
+
+a = input()
+a = int(a)
+
+if a<0:
+  print("Error")
+elif a>=0:
+  print(a*a)
