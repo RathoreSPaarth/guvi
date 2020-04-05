81ceac63b197f996c972896f3af0e7af075e7b9b
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2020-04-05 21:24:08 +0530

    Update codekata2.py

--- codekata_absBegin_python/codekata2.py
+++ codekata_absBegin_python/codekata2.py
@@ -1,4 +1,15 @@
-You are provided with two numbers. Find and print the smaller number.
+# You are provided with two numbers. Find and print the smaller number.
 
-Input Description:
-You are provided with two numbers as input.
+# Input Description:
+# You are provided with two numbers as input.
+inp = input()
+
+inp = inp.split()
+
+a = int(inp[0])
+b = int(inp[1])
+
+if a>b:
+    print(b)
+elif b>a:
+    print(a)
