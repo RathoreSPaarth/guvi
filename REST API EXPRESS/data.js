e3bbab0dfd9ef389dae4717df301bbabef933600
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2020-07-10 18:48:50 +0530

    Create data.js

---
+++ REST API EXPRESS/data.js
@@ -0,0 +1,48 @@
+let teachers = [
+    {
+        id: 1,
+        firstName: "Ankur",
+        lastName: "Narayan",
+        email: "ankur@gmail.com",
+        age: 25,
+        class: 5,
+        students: [
+            'shubham','varun','mathews','sunil'
+        ]
+    },
+    {
+        id: 2,
+        firstName: "Raman",
+        lastName: "Negi",
+        email: "negir@gmail.com",
+        age: 35,
+        class: 3,
+        students: [
+            'shubh','arun','mathews','sunil pal'
+        ]
+    },
+    {
+        id: 3,
+        firstName: "Swati",
+        lastName: "Narayan",
+        email: "swati@gmail.com",
+        age: 20,
+        class: 1,
+        students: [
+            'shubham','varun','mathews','sunil'
+        ]
+    },
+    {
+        id: 4,
+        firstName: "Ankit",
+        lastName: "chauhan",
+        email: "ankit@gmail.com",
+        age: 25,
+        class: 5,
+        students: [
+            'ashish','vaibhav','matt','sunny'
+        ]
+    }
+]
+
+module.exports = teachers;
